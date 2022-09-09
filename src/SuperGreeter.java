507b6c6ef592428ee512ae11cdec465d69f0ce48
HostilePlayer <mart5e86@stud.kea.dk>
2022-09-09 13:42:04 +0200

    tilføjet greeter

--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -1,4 +1,9 @@
+import java.util.Scanner;
+
 public class SuperGreeter {
+
+    static Scanner scanner = new Scanner(System.in);
+    String brugerNavn = "brugernavn";
 
     public void start(){
 
@@ -8,7 +13,14 @@ public class SuperGreeter {
     public  static void main(String[] args) {
 
         SuperGreeter greeter = new SuperGreeter();
+        hejBruger();
         greeter.start();
-
     }
+
+    private static void hejBruger() {
+        System.out.println("Hej bruger, hvad er dit navn?");
+        String brugerNavn = scanner.next();
+        System.out.println("hej " + brugerNavn);
+    }
+
 }
