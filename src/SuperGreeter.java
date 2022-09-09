8c028f996f7151bcb0b5995856b383755e918ec4
HostilePlayer <mart5e86@stud.kea.dk>
2022-09-09 13:46:00 +0200

    tilføjet askAboutAge

--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class SuperGreeter {
 
     static Scanner scanner = new Scanner(System.in);
-    String brugerNavn = "brugernavn";
+    static String brugerNavn = "brugernavn";
 
     public void start(){
 
@@ -14,13 +14,22 @@ public class SuperGreeter {
 
         SuperGreeter greeter = new SuperGreeter();
         hejBruger();
+        askAboutAge();
         greeter.start();
+    }
+
+    private static void askAboutAge() {
+        System.out.println("hvor gammel er du " + brugerNavn);
+        int brugerAlder = scanner.nextInt();
+        System.out.println("så du er " + brugerAlder + " år gammel");
+
     }
 
     private static void hejBruger() {
         System.out.println("Hej bruger, hvad er dit navn?");
         String brugerNavn = scanner.next();
         System.out.println("hej " + brugerNavn);
+
     }
 
 }
